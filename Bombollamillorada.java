9cc1e8e758ad5f691a965a8a543582ab83bc26a5
michael.J <79923488+michaelafk@users.noreply.github.com>
2022-06-27 11:26:58 +0200

    Update Bombollamillorada.java

--- Bombollamillorada.java
+++ Bombollamillorada.java
@@ -25,25 +25,47 @@ public class Bombollamillorada {
     public void inicio() {
         Random m = new Random();
         Cartes carta[] = new Cartes[10];
+        Cartesv2 cartav2[] = new Cartesv2[10];
         for (int i = 0; i < carta.length; i++) {
-            carta[i] = new Cartes(tipoCarta.values()[m.nextInt(4)].name(), m.nextInt(12));
+            int aux = m.nextInt(4);
+            int aux2 = m.nextInt(12) + 1;
+            carta[i] = new Cartes(tipoCarta.values()[aux].name(), aux2);
+            cartav2[i] = new Cartesv2(tipoCarta.values()[aux].name(), aux2);
         }
         for (int i = 0; i < carta.length; i++) {
             System.out.print(carta[i]);
         }
+        System.out.println("\n");
+        for (int i = 0; i < carta.length; i++) {
+            System.out.print(cartav2[i]);
+        }
         int n = carta.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = n-2; j >= i; j--) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = n - 2; j >= i; j--) {
                 if (carta[j + 1].menorque(carta[j])) {
-                    Cartes aux = carta[j+1];
-                    carta[j+1] = carta[j];
+                    Cartes aux = carta[j + 1];
+                    carta[j + 1] = carta[j];
                     carta[j] = aux;
+                }
+            }
+        }
+        int n2= cartav2.length;
+        for (int i = 0; i < n2 - 1; i++) {
+            for (int j = n2 - 2; j >= i; j--) {
+                if (cartav2[j + 1].menorque(cartav2[j])) {
+                    Cartesv2 aux = cartav2[j + 1];
+                    cartav2[j + 1] = cartav2[j];
+                    cartav2[j] = aux;
                 }
             }
         }
         System.out.println("\n");
         for (int i = 0; i < carta.length; i++) {
             System.out.print(carta[i]);
+        }
+        System.out.println("\n");
+        for (int i = 0; i < carta.length; i++) {
+            System.out.print(cartav2[i]);
         }
 
     }
