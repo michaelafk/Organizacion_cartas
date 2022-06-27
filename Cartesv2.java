08fd1b507acbbcef85b7d7360bf2a322ee499794
michael.J <79923488+michaelafk@users.noreply.github.com>
2022-06-27 11:27:47 +0200

    Update Cartesv2.java

--- Cartesv2.java
+++ Cartesv2.java
@@ -16,22 +16,23 @@ public class Cartesv2 {
     private int numero;
 
     //metodo constructor vacio
-    public Cartes() {
+    public Cartesv2() {
+
     }
 
     //metodo constructor con parametros
-    public Cartes(String pal, int numero) {
-        this.pal = pal;
+    public Cartesv2(String pal, int numero) {
+        this.pal = tipoCarta.valueOf(pal);
         this.numero = numero;
     }
     //metodos gets y sets
 
-    public String getPal() {
+    public tipoCarta getPal() {
         return pal;
     }
 
     public void setPal(String pal) {
-        this.pal = pal;
+        this.pal = tipoCarta.valueOf(pal);
     }
 
     public int getNumero() {
@@ -43,57 +44,18 @@ public class Cartesv2 {
     }
 
     //metodo menorque
-    public boolean menorque(Cartes a) {
-        boolean aux2 = false;
-        switch (pal) {
-            case "OROS":
-                if (a.getPal().equals("BASTOS") || a.getPal().equals("COPES") || a.getPal().equals("ESPASES")) {
-                    aux2=true;
-                } else {
-                    if (a.getNumero() > numero) {
-                        aux2=true;
-                    } else {
-                        aux2=false;
-                    }
-                }
-                break;
-            case "BASTOS":
-                if (a.getPal().equals("COPES") || a.getPal().equals("ESPASES")) {
-                    aux2=true;
-                } else if (a.getPal().equals("OROS")) {
-                    aux2=false;
-                } else {
-                    if (a.getNumero() > numero) {
-                        aux2=true;
-                    } else {
-                        aux2=false;
-                    }
-                }
-                break;
-            case "COPES":
-                if (a.getPal().equals("ESPASES")) {
-                    aux2=true;
-                } else if (a.getPal().equals("OROS") || a.getPal().equals("BASTOS")) {
-                    aux2=false;
-                } else {
-                    if (a.getNumero() > numero) {
-                       aux2=true;
-                    } else {
-                       aux2=false;
-                    }
-                }
-                break;
-            case "ESPASES":
-                if (a.getPal().equals("OROS") || a.getPal().equals("BASTOS") || a.getPal().equals("COPES")) {
-                    aux2=false;
-                } else {
-                    if (a.getNumero() > numero) {
-                        aux2=true;
-                    } else {
-                        aux2=false;
-                    }
-                }
-                break;
+    public boolean menorque(Cartesv2 a) {
+        boolean aux2;
+        if (pal.ordinal() < a.getPal().ordinal()) {
+            aux2 = true;
+        } else if (pal.ordinal() > a.getPal().ordinal()) {
+            aux2 = false;
+        } else {
+            if (numero < a.getNumero()) {
+                aux2 = true;
+            } else {
+                aux2 = false;
+            }
         }
         return aux2;
     }
@@ -101,7 +63,7 @@ public class Cartesv2 {
 
     @Override
     public String toString() {
-        return "{" + pal + numero + "}";
+        return "{" + pal.name() + numero + "}";
     }
 
 }
